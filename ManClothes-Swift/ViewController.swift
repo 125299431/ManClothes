437f37602888cafa70e005058bf58a6a5af3dec2
hongye <125299431>
2016-03-15 10:17:45 +0800

    ManClothes-Swift

---
+++ ManClothes-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManClothes-Swift
+//
+//  Created by ZhengHongye on 16/3/15.
+//  Copyright © 2016年 zhenghongye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
